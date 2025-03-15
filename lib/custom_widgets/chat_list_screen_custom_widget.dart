b4398a320ccb77bedd6f8b025dfca9746d22fb12
JackGilinger <therzzanfcsty@gmail.com>
2025-03-15 19:09:14 +0300

    Create chat_list_screen_custom_widget.dart

---
+++ lib/custom_widgets/chat_list_screen_custom_widget.dart
@@ -0,0 +1,31 @@
+// Automatic FlutterFlow imports
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/custom_code/widgets/index.dart'; // Imports other custom widgets
+import '/flutter_flow/custom_functions.dart'; // Imports custom functions
+import 'package:flutter/material.dart';
+// Begin custom widget code
+// DO NOT REMOVE OR MODIFY THE CODE ABOVE!
+
+class ChatListScreenCustomWidget extends StatefulWidget {
+  const ChatListScreenCustomWidget({
+    super.key,
+    this.width,
+    this.height,
+  });
+
+  final double? width;
+  final double? height;
+
+  @override
+  State<ChatListScreenCustomWidget> createState() =>
+      _ChatListScreenCustomWidgetState();
+}
+
+class _ChatListScreenCustomWidgetState
+    extends State<ChatListScreenCustomWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
